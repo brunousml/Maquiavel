b130730fa0759906457e57a0e0fe9336a3afda12
Bruno Melo <brunousml@gmail.com>
2017-05-22 16:11:47 -0300

    refactor parliamentarians requester

--- lib/parliamentarians/requester.rb
+++ lib/parliamentarians/requester.rb
@@ -5,27 +5,30 @@ require 'json'
 
 module Parliamentarians
   class Requester
-    def initialize
-      @@uri_actual_list = URI.parse("http://legis.senado.leg.br/dadosabertos/senador/lista/atual")
+    def initialize(url)
+      @@uri_actual_list = URI.parse(url)
     end
 
     def get_json_raw
-      # @raw_data = Net::HTTP.get(URI.parse(@@uri_actual_list))
-      @resp = Net::HTTP.new(@@uri_actual_list.host,
-                           @@uri_actual_list.port).start {|http| http.request(create_json_request) }
-    end
-
-    def parse
-      @resp ||= get_json_raw
-      @list = JSON.parse(@resp.body)
+      do_request(create_json_request)
     end
 
     def length
-      @list ||= parse
+      @list ||= json_to_hash
       @list['ListaParlamentarEmExercicio']['Parlamentares']['Parlamentar'].length
     end
 
     private
+    def do_request(req)
+      @resp = Net::HTTP.new(@@uri_actual_list.host,
+                            @@uri_actual_list.port).start {|http| http.request(req) }
+    end
+
+    def json_to_hash
+      @resp ||= get_json_raw
+      @list = JSON.parse(@resp.body)
+    end
+
     def create_json_request
       header = {'Content-Type' =>'application/json'}
       req = Net::HTTP::Get.new(@@uri_actual_list,
--- test/unit/test_parliamentarians_requester.rb
+++ test/unit/test_parliamentarians_requester.rb
@@ -1,11 +1,12 @@
 require_relative '../../lib/parliamentarians/requester'
+require_relative '../../bin/settings'
 require 'test/unit'
 
 module TestParliamentarians
   class TestParliamentariansRequester < Test::Unit::TestCase
     def test_get_json_raw
       # Setup
-      req = Parliamentarians::Requester.new
+      req = Parliamentarians::Requester.new(PARLIAMENTARIANS_URL)
 
       # Act
       body = req.get_json_raw
