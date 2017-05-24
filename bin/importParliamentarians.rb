2d28dc6817272977e845cd1aa6f07603bb78aabb
Bruno Melo <brunousml@gmail.com>
2017-05-24 10:27:37 -0300

    add import parliamentarians script

---
+++ bin/importParliamentarians.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/parliamentarians/extract/extractor'
+require_relative '../lib/settings'
+
+puts "Started crawl parliamentarians"
+parliamentarians = Parliamentarians::Extractor.new(ENV)
+data = parliamentarians.dump
+
+puts data
+puts "Data Imported"
