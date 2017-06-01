fca65ec8438238b5325377ee0db02ef6ef999858
Bruno Melo <brunousml@gmail.com>
2017-06-01 15:53:33 -0300

    create Parliamentarians::Transformer

---
+++ test/unit/transform/test_parliamentarians_transformer.rb
@@ -0,0 +1,22 @@
+require_relative '../../../lib/parliamentarians/transform/transformer'
+require_relative '../../../lib/settings'
+require 'test/unit'
+
+# This unit test is related to Parliamentarians::Transformer class
+module TestParliamentarians
+  class TestTransformer < Test::Unit::TestCase
+
+    # Test save data transformed into db
+    def test_save
+      # Setup
+      transformer = Parliamentarians::Transformer.new
+
+      # Act
+      saved_number = transformer.save
+
+      # Assertion
+      assert_not_nil(saved_number)
+      assert_equal(81, saved_number)
+    end
+  end
+end
